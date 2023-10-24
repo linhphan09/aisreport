d1dd9e8555b489c6c322532f14853ccf620db173
Linh Phan <148796792+linhphan09@users.noreply.github.com>
2023-10-24 22:21:27 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -2,16 +2,11 @@ import streamlit as st
 import pandas as pd
 import psycopg2
 
-@st.cache_resource
-def init_connection():
-    return psycopg2.connect(**st.secrets["postgresql"])
-conn = init_connection()
-@st.experimental_memo(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-rows = run_query("SELECT * FROM customers LIMIT 10")
-data = pd.DataFrame(rows)
-data.columns=['ssn','first_name','country']
+# Initialize connection.
+conn = st.experimental_connection("postgresql", type="sql")
+
+# Perform query.
+df = conn.query('SELECT * FROM customers LIMIT 10;', ttl="10m")
+df = pd.DataFrame(rows)
+df.columns=['ssn','first_name','country']
 st.table(data)
